d277640c9340eaea6c5f4b3d0d7b1d6404db0973
IKAAM <97133723+IKAAMYT@users.noreply.github.com>
2026-06-26 11:45:09 +0200

    Add files via upload

--- src/client/component/updater.cpp
+++ src/client/component/updater.cpp
@@ -1,17 +1,53 @@
 #include <std_include.hpp>
-
+#include "loader/component_loader.hpp"
 #include "updater.hpp"
-#include "updater_ui.hpp"
-#include "file_updater.hpp"
+#include "game/game.hpp"
+
+#include <utils/flags.hpp>
+#include <utils/progress_ui.hpp>
+#include <updater/updater.hpp>
 
 namespace updater {
-void run(const std::filesystem::path &base) {
-  const auto self = utils::nt::library::get_by_address(run);
-  const auto self_file = self.get_path();
+void update() {
+  if (utils::flags::has_flag("noupdate")) {
+    return;
+  }
 
-  updater_ui updater_ui{};
-  const file_updater file_updater{updater_ui, base, self_file};
-
-  file_updater.run();
+  try {
+    run(game::get_appdata_path());
+  } catch (update_cancelled &) {
+    TerminateProcess(GetCurrentProcess(), 0);
+  } catch (const std::exception &e) {
+    utils::progress_ui::show_error("Updater Error", e.what());
+  } catch (...) {
+    utils::progress_ui::show_error("Updater Error",
+                                   "Unknown error occurred during update.");
+  }
 }
+
+class component final : public generic_component {
+public:
+  component() {
+    this->update_thread_ = std::thread([] { update(); });
+  }
+
+  void pre_destroy() override { join(); }
+
+  void post_unpack() override { join(); }
+
+  component_priority priority() const override {
+    return component_priority::updater;
+  }
+
+private:
+  std::thread update_thread_{};
+
+  void join() {
+    if (this->update_thread_.joinable()) {
+      this->update_thread_.join();
+    }
+  }
+};
 } // namespace updater
+
+REGISTER_COMPONENT(updater::component)
